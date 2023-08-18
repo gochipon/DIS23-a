6371d0a64f61279e27e762775b896f024d4a7687
hiroki-oist <hiroki.sawada1@oist.jp>
2023-08-18 15:34:16 +0900

    New Class "Goal" added

--- DietApp/app/src/main/java/com/example/dietapp/GoalSetting.java
+++ DietApp/app/src/main/java/com/example/dietapp/GoalSetting.java
@@ -7,18 +7,31 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import android.widget.EditText;
 
 public class GoalSetting extends AppCompatActivity {
+
+
+    private EditText targetWeightEditText;
+    private EditText targetDurationEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_goal_setting);
 
+        targetWeightEditText = findViewById(R.id.targetWeightEditText);
+        targetDurationEditText = findViewById(R.id.targetDurationEditText);
         Button goToGameTopPageButton = findViewById(R.id.goToGameTopPageButton);
         goToGameTopPageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                float targetWeight = Float.parseFloat(targetWeightEditText.getText().toString());
+                String targetDuration = targetDurationEditText.getText().toString();
+
+                Goal goal = new Goal(targetWeight, targetDuration);
+
                 Intent intent = new Intent(GoalSetting.this, GameTopPage.class);
                 startActivity(intent);
             }
