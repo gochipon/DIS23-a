b767de50eb42123a2d5b037fb6c1e96dbaff740e
koki-ota0 <kokiota2000@gmail.com>
2023-08-18 14:37:43 +0900

    カメラ機能の追加

--- DietApp/app/src/main/java/com/example/dietapp/GameTopPage.java
+++ DietApp/app/src/main/java/com/example/dietapp/GameTopPage.java
@@ -1,22 +1,50 @@
 package com.example.dietapp;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
 
-import com.example.dietapp.databinding.ActivityGameTopPageBinding;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class GameTopPage extends AppCompatActivity {
 
-    private ActivityGameTopPageBinding binding;
+    private Button morningButton, noonButton, nightButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_top_page);
-//
-//        float weightValue = MainActivity.weight;
-//
-//        binding.weightActivity.setText("Weight: " + weightValue + "kg");
+
+        // 各ボタンのリスナーをセット
+        morningButton = findViewById(R.id.morningButton);
+        morningButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToPictureInput();
+            }
+        });
+
+        noonButton = findViewById(R.id.noonButton);
+        noonButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToPictureInput();
+            }
+        });
+
+        nightButton = findViewById(R.id.nightButton);
+        nightButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToPictureInput();
+            }
+        });
+    }
+
+    // PictureInputページへの遷移処理
+    private void goToPictureInput() {
+        Intent intent = new Intent(GameTopPage.this, Picture_input.class);
+        startActivity(intent);
     }
 }
