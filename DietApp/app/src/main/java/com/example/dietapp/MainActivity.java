d91a5e82c4d38ef87ae543554343ec34e8ea28ec
hiroki-oist <hiroki.sawada1@oist.jp>
2023-08-18 14:33:34 +0900

    Conflict resolved

--- DietApp/app/src/main/java/com/example/dietapp/MainActivity.java
+++ DietApp/app/src/main/java/com/example/dietapp/MainActivity.java
@@ -42,7 +42,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-<<<<<<< HEAD
         Button goToGoalSettingButton = findViewById(R.id.goToGoalSettingButton);
 
 
@@ -52,57 +51,6 @@ public class MainActivity extends AppCompatActivity {
         weightEditText = findViewById(R.id.weightEditText);
         heightEditText = findViewById(R.id.heightEditText);
 
-=======
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        weightEditText = binding.weightEditText;
-        heightEditText = binding.heightEditText;
-        ageEditText    = binding.ageEditText;
-        String ageString = ageEditText.getText().toString();
-        String weightString = weightEditText.getText().toString();
-        String heightString = heightEditText.getText().toString();
-        age = Integer.parseInt(ageString);
-        height = Float.parseFloat(ageString);
-        weight = Float.parseFloat(ageString);
-
-        final TextView genderHint = findViewById(R.id.genderHint);
-
-        Spinner genderSpinner = findViewById(R.id.genderSpinner);
-        genderSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
-                // ここに選択されたときのアクションを書く（必要であれば）
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parentView) {
-                // ここに何も選択されていない場合のアクションを書く（必要であれば）
-            }
-        });
-
-        Spinner activityLevelSpinner = findViewById(R.id.activityLevelSpinner);
-        activityLevelSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
-                // ここに選択されたときのアクションを書く（必要であれば）
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parentView) {
-                // ここに何も選択されていない場合のアクションを書く（必要であれば）
-            }
-        });
-
-        Button goToGoalSettingButton = findViewById(R.id.goToGoalSettingButton);
-        goToGoalSettingButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, GoalSetting.class);
-                startActivity(intent);
-            }
-        });
->>>>>>> 4a421f489200005aa7fae1dd6a4a1709bb1bf680
 
         goToGoalSettingButton.setOnClickListener(new View.OnClickListener() {
             @Override
