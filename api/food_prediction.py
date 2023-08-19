437b6e52f11be56b3a1e088dd3145c43ae47e0ba
hiroki-oist <hiroki.sawada1@oist.jp>
2023-08-19 11:15:36 +0900

    Added CurryRice and FrenchToast

--- api/food_prediction.py
+++ api/food_prediction.py
@@ -10,7 +10,7 @@ import requests
 
 def predictFood(path_to_image):
     m = hub.KerasLayer('https://tfhub.dev/google/aiy/vision/classifier/food_V1/1')
-    image = iio.imread(path_to_image)
+    image = iio.imread(path_to_image)[..., :3]
     labelmap_url = "https://www.gstatic.com/aihub/tfhub/labelmaps/aiy_food_V1_labelmap.csv"
     input_shape = (224, 224)
 
@@ -28,7 +28,7 @@ def predictFood(path_to_image):
     return classes[predicted_index]
 
 def load_config():
-    with open("../config.json", "r") as file:
+    with open("./config.json", "r") as file:
         config = json.load(file)
     return config
 
@@ -57,3 +57,7 @@ def get_calories_for_dish(dish_name, app_id=food_app_id, app_key=food_app_key):
     
     # If there's an error or unexpected structure
     raise ValueError(f"Could not find calorie data for {dish_name}")
+
+# path_to_image = "/home/hirokisawada/Desktop/CurryRice.png"
+# predictedFood = predictFood(path_to_image)
+# get_calories_for_dish(predictedFood)
