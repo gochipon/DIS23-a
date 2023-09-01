96ad2d89f919767f79638f68cac19426a12ffd29
hiroki-oist <hiroki.sawada1@oist.jp>
2023-09-02 02:59:08 +0900

    Restructure the game top page

--- DietApp/app/src/main/java/com/example/dietapp/GameTopPage.java
+++ DietApp/app/src/main/java/com/example/dietapp/GameTopPage.java
@@ -12,6 +12,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -22,6 +23,7 @@ import android.widget.TextView;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
 import com.squareup.picasso.Picasso;
 
 import org.json.JSONException;
@@ -153,6 +155,20 @@ public class GameTopPage extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_top_page);
+
+        // 背景画像の最大化
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int screenWidth = displayMetrics.widthPixels;
+        int screenHeight = displayMetrics.heightPixels;
+
+        ImageView backGround = findViewById(R.id.backGround);
+
+        Glide.with(this)
+                .load(R.drawable.background)
+                .centerCrop()
+                .override(screenWidth, screenHeight)
+                .into(backGround);
 
         TextView morningCaloriesTextView = findViewById(R.id.morningCaloriesTextView);
         morningCaloriesTextView.setText(String.valueOf(Food.getBreakfastCalories()) + " kcal");
--- DietApp/app/src/main/java/com/example/dietapp/MainActivity.java
+++ DietApp/app/src/main/java/com/example/dietapp/MainActivity.java
@@ -10,11 +10,17 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.navigation.ui.AppBarConfiguration;
+import android.content.SharedPreferences;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.dietapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String PREFS_NAME = "MyPrefs";
+    private static final String KEY_FIRST_RUN = "firstRun";
 //
 //    private AppBarConfiguration appBarConfiguration;
     private Spinner genderSpinner;
@@ -27,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
     public static User user;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -40,17 +46,52 @@ public class MainActivity extends AppCompatActivity {
         heightEditText = findViewById(R.id.heightEditText);
         usernameEditText = findViewById(R.id.usernameEditText);
 
+        // 初回起動の判定ロジック
+        SharedPreferences prefs = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        boolean isFirstRun = prefs.getBoolean(KEY_FIRST_RUN, true);
 
-        goToGoalSettingButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                user = saveUserInfo();
+        if (isFirstRun) {
+            // 初回起動時の処理を行うためのActivityを起動
+            // 例: 初回起動時の情報入力画面に遷移する
+//            startActivity(FirstRunActivity.newIntent(this));
+
+            goToGoalSettingButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    user = saveUserInfo();
 
 
-                Intent intent = new Intent(MainActivity.this, GoalSetting.class);
-                startActivity(intent);
-            }
-        });
+                    Intent intent = new Intent(MainActivity.this, GoalSetting.class);
+                    startActivity(intent);
+                }
+            });
+
+            // 初回起動フラグをfalseに設定して保存
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putBoolean(KEY_FIRST_RUN, false);
+            editor.apply();
+        } else {
+            // 通常の起動時の処理を行う
+            // 例: アプリのメイン画面に遷移する
+            goToGoalSettingButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    user = saveUserInfo();
+
+
+                    Intent intent = new Intent(MainActivity.this, GoalSetting.class);
+                    startActivity(intent);
+                }
+            });
+
+            // 初回起動フラグをfalseに設定して保存
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putBoolean(KEY_FIRST_RUN, false);
+            editor.apply();
+        }
+
+
+
     }
 
         private User saveUserInfo() {
